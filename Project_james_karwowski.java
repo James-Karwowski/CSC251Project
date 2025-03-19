688a8b69c70fad5a4415f4c59e7dbd0d030d6d68
James <james.karwowski@outlook.com>
2025-03-19 08:08:54 -0400

    Update Project_james_karwowski.java
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_james_karwowski.java
+++ Project_james_karwowski.java
@@ -42,5 +42,8 @@ public class Demo{
             input.nextLine();
    		}
 		input.close();
+		} finally {
+		System.out.println("The number of policies with a smoker is: " + numSmoke);
+		System.out.println("The number of policies with a non-smoker is: " + (total-numSmoke));
 	}
 }
